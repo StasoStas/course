af0d1468e051ad9abcd33b8cf9b937468ee98fc2
StasoStas <79196346+StasoStas@users.noreply.github.com>
2021-03-01 19:37:48 +0200

    Add files via upload

---
+++ Lesson1/less_1/less_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name} , сегодня {DateTime.Now}");
+        }
+    }
+}
